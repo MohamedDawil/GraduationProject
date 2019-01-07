fcc1da95dc64e36c98bf3912e36ed9a81b3ac1e0
Mohamed <mohamed.a.dawil@outlook.com>
2019-01-07 16:00:40 +0100

    Add Search fix.

--- GraduationProject/Controllers/ReceiversController.cs
+++ GraduationProject/Controllers/ReceiversController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraduationProject.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraduationProject.Controllers
@@ -17,7 +18,26 @@ namespace GraduationProject.Controllers
         [HttpGet]
         public IActionResult Search()
         {
-            return View();
+            var viewModels = new ReceiversSearchVM
+            {
+                Products = new ReceiversProductVM[] {new ReceiversProductVM()}
+            };
+            return View(viewModels);
+        }
+
+        [HttpPost]
+        public IActionResult Search(ReceiversSearchVM receiversSearchVM)
+        {
+            if (!ModelState.IsValid)
+                return View(receiversSearchVM);
+
+            return RedirectToAction(nameof(Search));
+        }
+
+        [HttpPost]
+        public IActionResult ClaimProduct(int productId)
+        {
+            return RedirectToAction(nameof(Search));
         }
     }
 }
