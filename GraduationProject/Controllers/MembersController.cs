b3f0d200a09496240476dda624bd48a1dd80528a
Mohamed <mohamed.a.dawil@outlook.com>
2019-01-07 11:12:10 +0100

    Add Index.

---
+++ GraduationProject/Controllers/MembersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduationProject.Controllers
+{
+    public class MembersController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ GraduationProject/Models/ViewModels/MembersIndexVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduationProject.Models.ViewModels
+{
+    public class MembersIndexVM
+    {
+        [Required]
+        [Display(Name ="E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name ="Lösenord")]
+        public string Password { get; set; }
+
+        [Display(Name ="Kom ihåg mig")]
+        public bool RememberMe { get; set; }
+    }
+}
