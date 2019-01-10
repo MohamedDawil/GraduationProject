2703683a4d1cb16963edcd8ed74e535c4f53cb0f
Mohamed <mohamed.a.dawil@outlook.com>
2019-01-10 14:41:26 +0100

    Added database project

---
+++ GraduationProject.Database/dbo/Tables/AspNetUsers.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]                   NVARCHAR (450)     NOT NULL,
+    [UserName]             NVARCHAR (256)     NULL,
+    [NormalizedUserName]   NVARCHAR (256)     NULL,
+    [NormalizedEmail]      NVARCHAR (256)     NULL,
+    [EmailConfirmed]       BIT                NOT NULL,
+    [PasswordHash]         NVARCHAR (MAX)     NULL,
+    [SecurityStamp]        NVARCHAR (MAX)     NULL,
+    [ConcurrencyStamp]     NVARCHAR (MAX)     NULL,
+    [PhoneNumber]          NVARCHAR (MAX)     NULL,
+    [PhoneNumberConfirmed] BIT                NOT NULL,
+    [TwoFactorEnabled]     BIT                NOT NULL,
+    [LockoutEnd]           DATETIMEOFFSET (7) NULL,
+    [LockoutEnabled]       BIT                NOT NULL,
+    [AccessFailedCount]    INT                NOT NULL,
+    [Email]                NVARCHAR (256)     NULL,
+    [FirstName]            NVARCHAR (MAX)     NULL,
+    [LastName]             NVARCHAR (MAX)     NULL,
+    [Street]               NVARCHAR (MAX)     NULL,
+    [ZipCode]              NVARCHAR (MAX)     NULL,
+    [City]                 NVARCHAR (MAX)     NULL,
+    [Picture]              NVARCHAR (MAX)     NULL,
+    CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
+    ON [dbo].[AspNetUsers]([NormalizedUserName] ASC) WHERE ([NormalizedUserName] IS NOT NULL);
+
+
+GO
+CREATE NONCLUSTERED INDEX [EmailIndex]
+    ON [dbo].[AspNetUsers]([NormalizedEmail] ASC);
+
---
+++ GraduationProject.Database/fresh/Tables/product.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [fresh].[product] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (256) NOT NULL,
+    [Freshness]   INT            NOT NULL,
+    [ExpiryDate]  DATETIME2 (7)  NOT NULL,
+    [Description] NVARCHAR (MAX) NOT NULL,
+    [PickUpDate1] DATETIME2 (7)  NOT NULL,
+    [PickUpDate2] DATETIME2 (7)  NOT NULL,
+    [Claimed]     BIT            NOT NULL,
+    [Collected]   BIT            NOT NULL,
+    [Picture]     NVARCHAR (MAX) NOT NULL,
+    [PublishDate] DATETIME2 (7)  NOT NULL,
+    [GiverId]     NVARCHAR (450) NOT NULL,
+    [ReceiverId]  NVARCHAR (450) NULL,
+    [Latitude]    NVARCHAR (MAX) NOT NULL,
+    [Longitude]   NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([GiverId]) REFERENCES [dbo].[AspNetUsers] ([Id]),
+    FOREIGN KEY ([ReceiverId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
